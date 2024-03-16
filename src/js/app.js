c7de2c9d8d75b411886bb24a554766816b92bd5e
Ludiamen <maxvas1001@gmail.com>
2024-03-16 23:02:45 +0300

    add files to project

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+// export default class Validator {
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     validateUsername() {
+//         return (/^[^\d-_][\w-]+[^\d-_]$/.test(this.name)) && !(/\d{4,}/.test(this.name));
+//     }
+// }
+
+
+export default class Validator {
+    validateUsername(username) {
+      // Паттерн для проверки имени пользователя
+      const pattern = /^[a-zA-Z0-9_-]+$/;
+  
+      // Проверяем, соответствует ли имя паттерну
+      if (pattern.test(username)) {
+        // Проверка на подряд идущие цифры
+        if (!/\d{4,}/.test(username)) {
+          // Проверка на начало и конец с цифры, подчёркивания или тире
+          if (!/^[0-9_-]|[_-]$/.test(username)) {
+            return true; // Имя пользователя валидно
+          }
+        }
+      }
+  
+      return false; // Имя пользователя не соответствует правилам
+    }
+  }
